21c4e2cbc3730ad219c8e07806699f69d467b44d
QuangTM <tranminhquang0907@gmail.com>
2022-02-21 15:02:33 +0700

    update

--- src/main/scala/HOF.scala
+++ src/main/scala/HOF.scala
@@ -2,13 +2,14 @@
  * Created by QuangTM on 21-Feb-2022.
  */
 object HOF extends App {
-  val sayHello2: (() => Unit) => Unit = f => f()
-  val helloQuang2: () => Unit = () => println("Hello, Quang2")
+  def sayHello(callback: () => Unit): Unit = callback()
+
+  def helloQuang(): Unit = println("Hello, Quang")
 
   sayHello(helloQuang)
 
-  def sayHello(callback: () => Unit): Unit = callback()
-  def helloQuang(): Unit = println("Hello, Quang")
+  val sayHello2: (() => Unit) => Unit = f => f()
+  val helloQuang2: () => Unit = () => println("Hello, Quang2")
 
   sayHello2(helloQuang2)
 }
