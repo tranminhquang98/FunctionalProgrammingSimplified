cc5feb465818c93f32276f0947c282f0ce5102a8
QuangTM <tranminhquang0907@gmail.com>
2022-05-21 10:38:03 +0700

    update

--- src/main/scala/ForComprehension.scala
+++ src/main/scala/ForComprehension.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.ArrayBuffer
+
 /**
  * Created by QuangTM on 18-May-2022.
  */
@@ -14,12 +16,48 @@ object ForComprehension extends App {
     if fname startsWith "b" // filter
   } yield fname.toUpperCase
 
-  namesStartingWithB.foreach(println)
+  case class Sequence[A](initialElems: A*) {
+    private val elems = ArrayBuffer[A]()
+    elems ++= initialElems
 
-  val oddNums = for {
-    i <- 1 to 10
-    x = i
-    if x % 2 != 0
-  } yield x
-  oddNums.foreach(println)
+    def foreach(block: A => Unit): Unit = elems.foreach(block)
+
+    def map[B](f: A => B): Sequence[B] = Sequence(elems.map(f): _*)
+
+    def flatMap[B](f: A => Sequence[B]): Sequence[B] = {
+      val mapRes: Sequence[Sequence[B]] = map(f) //map
+      flattenLike(mapRes) //flatten
+    }
+
+    def withFilter(p: A => Boolean): Sequence[A] = Sequence(elems.filter(p): _*)
+
+    def flattenLike[B](seqOfSeq: Sequence[Sequence[B]]): Sequence[B] = {
+      val xs = ArrayBuffer[B]()
+      for (listB: Sequence[B] <- seqOfSeq) {
+        for (e <- listB) {
+          xs += e
+        }
+      }
+      Sequence(xs: _*)
+    }
+  }
+
+  case class Person2(name: String)
+  val myFriends = Sequence(
+    Person2("Adam"),
+    Person2("David"),
+    Person2("Frank")
+  )
+  val adamsFriends = Sequence(
+    Person2("Nick"),
+    Person2("David"),
+    Person2("Frank")
+  )
+
+  val mutualFriends = for {
+    myFriend <- myFriends // generator
+    adamsFriend <- adamsFriends // generator
+    if myFriend.name == adamsFriend.name
+  } yield myFriend
+  mutualFriends.foreach(println)
 }
